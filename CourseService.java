ef4ff0ad95d05f99c4f5cadbb1b410c076d49acf
Nikhil Sharma <nikhilsharmanikk53@gmail.com>
2023-05-08 17:18:09 +0530

    Add files via upload

---
+++ CourseService.java
@@ -0,0 +1,16 @@
+package com.springrest.springrest.services;
+
+import java.util.List;
+
+import com.springrest.springrest.entities.Course;
+
+public interface CourseService {
+
+	public List<Course> getCourses();
+	
+	public Course getCourse(long courseId);
+
+//	public Course getCourse(long parseLong);
+	
+	public Course addCourse(Course course);
+}
